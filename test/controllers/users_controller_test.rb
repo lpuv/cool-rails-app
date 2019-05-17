551a878445e1c76666f171958fc410cd628b7e17
Gamer4Life Puvilland <uyenwontanswer@gmail.com>
2019-05-17 10:16:31 -0700

    Codecov test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -14,7 +14,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "should redirect edit when not logged in" do
     get edit_user_path(@user)
-    assert_not flash.empty?
+    assert flash.empty?
     assert_redirected_to login_url
   end
 
