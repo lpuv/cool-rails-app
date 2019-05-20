b4dd86b090f60b6a9411267ee4a6b9b215bb1a6e
Gamer4Life Puvilland <uyenwontanswer@gmail.com>
2019-05-20 12:15:57 -0700

    Captcha try 6

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  include RecaptchaVerifiable
   before_action :logged_in_user, only: %i[index edit update destroy]
   before_action :correct_user,   only: %i[edit update]
   before_action :admin_user,     only: :destroy
